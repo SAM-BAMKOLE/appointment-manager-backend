018465d8f7cc4a5ab6d3ce7e138a4be4ff53e60a
samBamkole <olagoroyesamuel7@gmail.com>
2025-08-06 15:09:39 +0100

    added backend url to cors whitelist

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ app.use(
         origin: process.env.ALLOWED_ORIGINS?.split(",") || [
             "http://localhost:3000",
             "http://localhost:5173",
+            "https://schedify-backend.onrender.com",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
